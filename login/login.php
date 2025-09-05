dd996bcf231e7d1cf03a0b83851b2decc99df469
carlinifdz <carlo.fernandez.4567@gmail.com>
2025-09-05 11:29:40 -0600

    conexion a phpmyadmin y login sin logica
    hola comostan

---
+++ login/login.php
@@ -0,0 +1,11 @@
+<html>
+    Usuario: <input type="text" id="username" name="username" placeholder="Introduce tu usuario"><br>
+    Contraseña: <input type="text" id="password" name="password" placeholder="Introduce tu contraseña"><br>
+
+    <button id="enter" name="enter">Entrar</button>
+    <?php
+    
+    echo "Ta jalando"
+    
+    ?>
+</html>
