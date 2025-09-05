2d61801e314e78a503839f7e0d0741705c53cd63
carlinifdz <carlo.fernandez.4567@gmail.com>
2025-09-05 12:23:42 -0600

    logica de login con hash
    hice el loggeo, el registro es temporal, en lo que nos pasan la base

--- login/login.php
+++ login/login.php
@@ -1,11 +1,65 @@
 <html>
-    Usuario: <input type="text" id="username" name="username" placeholder="Introduce tu usuario"><br>
-    Contraseña: <input type="text" id="password" name="password" placeholder="Introduce tu contraseña"><br>
+    <body>
+        <form method="post" action="">
+            Usuario: 
+            <input type="text" id="reg_username" name="reg_username" placeholder="Introduce tu usuario"><br>
+            Contraseña: 
+            <input type="password" id="reg_password" name="reg_password" placeholder="Introduce tu contraseña"><br>
 
-    <button id="enter" name="enter">Entrar</button>
-    <?php
-    
-    echo "Ta jalando"
-    
-    ?>
+            <button type="submit" name="register">Registro</button><br><br><br>
+
+            Usuario: 
+            <input type="text" id="log_username" name="log_username" placeholder="Introduce tu usuario"><br>
+            Contraseña: 
+            <input type="password" id="log_password" name="log_password" placeholder="Introduce tu contraseña"><br>
+
+            <button type="submit" name="login">Inicio de sesion</button>
+        </form>
+
+        <?php
+        include '../php/conn.php';
+
+        if (isset($_POST['login'])){
+            $usuario = $_POST['log_username'];
+            $contrasena = $_POST['log_password'];
+
+            $sql = "SELECT contrasena FROM usuarios WHERE usuario = ?";
+            $stmt = $conexion->prepare($sql);
+            mysqli_stmt_bind_param($stmt,"s",$usuario);
+            mysqli_stmt_execute($stmt);
+
+            $resultado = mysqli_stmt_get_result($stmt);
+
+            if ($row = mysqli_fetch_assoc($resultado)){
+                $hashGuardado = $row['contrasena'];
+
+                if (password_verify($contrasena,$hashGuardado)){
+                    echo "Iniciaste sesion";
+                }else{
+                    echo "Contraseña incorrecta";
+                }
+            }
+        }
+
+
+        if (isset($_POST['register'])) {
+            $usuario = $_POST['reg_username'];
+            $contrasena = $_POST['reg_password'];
+
+            $hash = reg_password_hash($contrasena, reg_PASSWORD_ARGON2ID);
+
+            $sql = "INSERT INTO usuarios (usuario, contrasena) VALUES (?, ?)";
+            $stmt = $conexion->prepare($sql);
+            $stmt->bind_param("ss", $usuario, $hash);
+
+            if ($stmt->execute()) {
+                echo "Usuario registrado correctamente.";
+            } else {
+                echo "Error: " . $conexion->error;
+            }
+
+            $stmt->close();
+        }
+        ?>
+    </body>
 </html>
--- php/conn.php
+++ php/conn.php
@@ -9,8 +9,6 @@ $conexion = new mysqli($server, $user, $pass, $db);
 
 if($conexion -> connect_errno){
     die("Conexion fallida!! ".$conexion -> connect_errno);
-}else{
-    echo"Conectado";
-}
+}else
 
 ?>
